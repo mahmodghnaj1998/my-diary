c658e79479144ffe6674f3253ea30330e206ff6e
mahmodghnaj1998 <mahmodghnaj1998@gmail.com>
2020-12-27 00:18:39 +0500

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.port || 8000;
 const app = express();
 
 app.use(express.static(__dirname + "/dist"));
-app.get(/.*/, function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(__dirname + "/dist/index.html")
 });
 app.listen(port)
